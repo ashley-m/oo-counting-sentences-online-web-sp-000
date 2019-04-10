de72f45b58b0decf37d7973d8af05e900f899816
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 00:58:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    tube = self.split(/[\s.?!,]/)
+    tube = self.split(/[\s,]/)
     count = 0
     tube.each do |x|
       count += 1 if x.question?||x.question?||x.exclamation?
